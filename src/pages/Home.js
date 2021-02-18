5569e4654c7a706febcb5eb7fc335b0e9cd9f7a8
Denis <dsapaiv@gmail.com>
2021-02-18 19:13:40 +0200

    Started working on the endless fetching

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,4 +1,4 @@
-import React, { useEffect } from 'react';
+import React, { useEffect, useRef, useState } from 'react';
 
 import AlbumList from '../components/AlbumList';
 import Layout from '../components/Layout';
@@ -11,18 +11,39 @@ import { setDoneFetching } from '../store/actions/albumsActions';
 export default function Home() {
   const dispatch = useDispatch();
 
-  const getAlbums = async () => {
+  const [page, setPage] = useState(0);
+  const loader = useRef(null);
+
+  const getAlbums = async (page = 0) => {
     const data = await getAllNewReleases(
-      0,
+      page,
       window.localStorage.getItem('token')
     );
     console.log(data);
     dispatch(setDoneFetching({ isFetching: false, data: data.albums.items }));
   };
 
+  const handleObserver = (entities) => {
+    const target = entities[0];
+    if (target.isIntersecting) {
+      setPage((page) => page + 1);
+    }
+  };
+
   useEffect(() => {
     getAlbums();
+    var options = {
+      root: null,
+      rootMargin: '20px',
+      threshold: 1.0,
+    };
+    const observer = new IntersectionObserver(handleObserver, options);
+    if (loader.current) {
+      observer.observe(loader.current);
+    }
   }, []);
+
+  useEffect(() => {}, []);
 
   return (
     <Layout>
